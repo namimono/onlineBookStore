06b7545a1421b3abcacd652ebb8cd4a558daf311
kami <42517157+kami@users.noreply.github.com>
2019-06-10 19:37:42 +0800

    编写了JsonUtil，使用的式JackSon中的ObjectMapper对象，可以用该工具类解析json字符串

--- src/main/java/com/hx5847/utils/JsonUtil.java
+++ src/main/java/com/hx5847/utils/JsonUtil.java
@@ -1,12 +1,25 @@
 package com.hx5847.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 public class JsonUtil {
-    private static ObjectMapper objectMapper=new ObjectMapper();
+    private static ObjectMapper mapper=new ObjectMapper();
 
     private JsonUtil(){
 
+    }
+    public static String generate(Object object) throws JsonProcessingException {
+        return mapper.writeValueAsString(object);
+    }
+
+    /**
+     * Deserialize JSON content from given JSON content String.
+     */
+    public static <T> T parse(String content, Class<T> valueType) throws IOException {
+        return mapper.readValue(content, valueType);
     }
 
 
